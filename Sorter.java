36683f1be2364cb7a1d9f83ead597ce26195d73f
afateel2 <aboodi.fateel3@gmail.com>
2023-02-07 16:18:10 +0300

    insertion sort

--- Sorter.java
+++ Sorter.java
@@ -3,20 +3,18 @@ public class Sorter {
     public static void main(String[] args) {
         
     }
-
-    public static void selectionSort(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++)  
-        {  
-            int index = i;  
-            for (int j = i + 1; j < arr.length; j++){  
-                if (arr[j] < arr[index]){  
-                    index = j;//searching for lowest index  
-                }  
-            }  
-            int smallerNumber = arr[index];   
-            arr[index] = arr[i];  
-            arr[i] = smallerNumber;  
-        }  
+    public static void insertionSort(int[] arr){
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+ 
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
     }
 
-}
